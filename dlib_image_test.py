6ed4fef52f9ada642ea4e32722c6f130c8b04b3d
yuf11235 <13220100318@163.com>
2020-03-19 22:32:43 +0800

    数学形态学来计算吧

--- dlib_image_test.py
+++ dlib_image_test.py
@@ -1,17 +1,36 @@
 import numpy as np
 import cv2
 import dlib
+import matplotlib.pyplot as plt
 
 # 加载算法模型
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('etc/shape_predictor_68_face_landmarks.dat')
 
 # 读取图像文件
-img = cv2.imread("data\girl.jpg")
+img = cv2.imread("data\\beauty.jpg")
+# img = cv2.imread("data\girl.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-# 转换为二值图
+"""   # 先定义一个元素结构
+r = 16
+h = w = r * 2 + 1
+kernel = np.zeros((h, w), dtype=np.uint8)
+cv2.circle(kernel, (r, r), r, 1, -1)
+# 开运算,去噪声
+openingimg = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
+cv2.imshow("openingimg_1", openingimg)
+# 获取差分图
+strtimg = cv2.absdiff(gray, openingimg)
+cv2.imshow("strtimg", strtimg)
 
+# 转换为二值图
+maxi = float(img.max())
+mini = float(img.min())
+x = maxi - ((maxi - mini) / 2)
+# 二值化,返回阈值ret  和  二值化操作后的图像thresh
+ret, thresh = cv2.threshold(strtimg, x, 255, cv2.THRESH_BINARY)
+cv2.imshow('binary', thresh)"""
 
 # 识别人脸区域
 rects = detector(gray, 1)
@@ -25,9 +44,12 @@ if len(rects) > 0:
         pos = (point[0], point[1])
         # 计算两个端点到中心的二值图的像素值的和，哪边大即为眼睛的方向
         #
+        print(pos)
         cv2.circle(img, pos, 1, color=(0, 255, 0))
         # font = cv2.FONT_HERSHEY_SIMPLEX
         # cv2.putText(img, str(idx + 1), pos, font, 0.4, (0, 255, 255), 1, cv2.LINE_AA)
+    plt.scatter(landmarks[:, 0], -landmarks[:, 1])
+    plt.show()
 cv2.imshow('landmarks', img)
 # 释放资源
 cv2.waitKey()
